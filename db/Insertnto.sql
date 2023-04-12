674742ddd49aa7053d66cf3272a70ebe140edeea
Anh467 <nguyenanhviet080702@gmail.com>
2023-04-12 17:08:51 +0700

    add

--- db/Insertnto.sql
+++ db/Insertnto.sql
@@ -1,13 +1,33 @@
-insert into dbo.users
+﻿insert into dbo.users
+(
+    user_name
+  , user_mail
+  , avatar_url
+  , introduce
+  , follower
+)
 values
-('Nguyen Anh Viet', 'van123872000@gmail.com', 'https://static01.nyt.com/images/2022/08/24/science/00SCI-SPIDERLIES-01/00SCI-SPIDERLIES-01-jumbo.jpg?quality=75&auto=webp', 'EYE of the STORM');
+(   N'Nguyễn Anh Việt' -- user_name - nvarchar(127)
+  , 'van123872000@gmail.com'  -- user_mail - varchar(255)
+  , 'https://hips.hearstapps.com/hmg-prod/images/womanyellingcat-1573233850.jpg'  -- avatar_url - varchar(1027)
+  , 'abc hehe'  -- introduce - varchar(255)
+  , 0   -- follower - int
+    )
 insert into dbo.userInfor
+(
+    user_id
+  , user_dob
+  , study_at
+  , work_at
+  , favorites
+  , created_at
+)
 values
-(   '4084eb8f-5db4-4685-a907-541cee494688'      -- user_id - uniqueidentifier
+(   'UID00000002'        -- user_id - varchar(11)
   , '2002-07-08' -- user_dob - date
   , 'fpt'        -- study_at - varchar(127)
   , 'fpt'        -- work_at - varchar(127)
-  , 'game game game'        -- favorites - varchar(255)
+  , 'badminton'        -- favorites - varchar(255)
   , getdate() -- created_at - datetime
     )
 insert into dbo.userAccount
@@ -17,37 +37,8 @@ insert into dbo.userAccount
   , user_password
 )
 values
-(   '4084eb8f-5db4-4685-a907-541cee494688' -- user_id - uniqueidentifier
-  , 'nguyenanhviet'   -- user_account - varchar(127)
-  , '533f91b7394a3bd14dac528ef217febebe69f80334cc8735694c2fa17e9a6a3d'   -- user_password - varchar(64)
+(   'UID00000002' -- user_id - varchar(11)
+  , 'viet080702' -- user_account - varchar(127)
+  , '$argon2i$v=19$m=65536,t=28,p=1$DpEvNSPwaUDDJUDfBwXENg$W1TENxDzzXutPFxY8pZi487lBT7YErXuG+eYqD67F20' -- user_password - varchar(100)
     )
-select * from dbo.userAccount
-where user_account= 'nguyenanhviet' and user_password= '533f91b7394a3bd14dac528ef217febebe69f80334cc8735694c2fa17e9a6a3d'
-
-insert into dbo.userBoxchat
-(
-    user_id
-  , boxchat_id
-)
-values
-(   '4084eb8f-5db4-4685-a907-541cee494688' -- user_id - uniqueidentifier
-  , '4084eb8f-5db4-4685-a907-541cee494688' -- boxchat_id - uniqueidentifier
-    )
-
-insert into dbo.post
-(
-    user_id
-  , boxchat_id
-  , post_id
-  , post_content
-  , post_status
-  , post_date
-)
-values
-(   '4084eb8f-5db4-4685-a907-541cee494688'      -- user_id - uniqueidentifier
-  , '4084eb8f-5db4-4685-a907-541cee494688'      -- boxchat_id - uniqueidentifier
-  , null      -- post_id - uniqueidentifier
-  , ''        -- post_content - varchar(255)
-  , 0         -- post_status - int
-  , getdate() -- post_date - date
-    )
+	
---
+++ db/query.sql
@@ -0,0 +1,5 @@
+select * from dbo.userAccount
+where user_account='viet080702'
+
+select * from dbo.userAccount
+where user_id='UID00000002'
